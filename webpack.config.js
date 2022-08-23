9c8396161bd9f8bfc07a3c38e0178a2735fcdd0c
JuanCRivas21 <juancrivas2179@gmail.com>
2022-08-23 16:39:53 -0500

    Primero

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// const path = require('path');
+
+//  module.exports = {
+//      entry: './src/index.js',
+//      devServer: {
+//         static: "./dist",
+//         hot: true,
+//       },
+//      output: {
+//          filename: 'main.js',
+//          path: path.resolve(__dirname, 'dist')
+//      }
+//  };
