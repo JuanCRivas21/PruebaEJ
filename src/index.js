eb47452a4e92a771a531828fa10d130787dbd4f0
JuanCRivas21 <juancrivas2179@gmail.com>
2022-08-24 22:11:29 -0500

    agregado el archivo readme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert ("hola")
+
+let numero1 = 15;
+const numero2= 11;
+
+function prueba() { 
+    var numero = 10;
+    numero1=5
+     console.log(numero,numero1,numero2) 
+  }
+  
+  console.log(numero1,numero2)
+
+  prueba();
