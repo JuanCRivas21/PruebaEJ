a016f7af50be23f86bf815abf72fc95a482c5e78
Juan Carlos <juancrivas2179@gmail.com>
2022-09-14 19:19:17 -0500

    ejercicio

--- src/JS/dos_numeros.js
+++ src/JS/dos_numeros.js
@@ -1,16 +1,20 @@
- const a = prompt('Hola, por favor ingresa el primer numero')
- const b = prompt ('hola por favor ingresa el segundo numero')
+/*const primer =prompt("Hola, por favor ingresa el primer numero")
+const segundo =prompt('hola por favor ingresa el segundo numero')
 
  function verificacion (){
-    if(a === 'b'){
+    if(primer === 'segundo'){
         alert('Los dos numeros son iguales')
     }
-    if(a < 'b'){
+    if(primer < 'segundo'){
         alert('el primer numero es menor')
     }
-    else if (a >'b'){
+    else if (primer >'segundo'){
         alert('el primer numero es mayor')
     }
  }
 
  verificacion()
+ */
+
+ let primer = prompt("Ingresa el primer numero")
+ let segundo = prompt("Ingresa el segundo numero")
--- src/index.js
+++ src/index.js
@@ -52,3 +52,21 @@ while(boton.toLowerCase() !=='s'){
     valor++;
 }
 */
+
+
+const primer =prompt("Hola, por favor ingresa el primer numero")
+const segundo =prompt('hola por favor ingresa el segundo numero')
+
+ function verificacion (){
+    if(primer === segundo){
+        alert('Los dos numeros son iguales')
+    }
+    if(primer < segundo){
+        alert('el segundo numero es mayor')
+    }
+    else if (primer >segundo){
+        alert('el primer numero es mayor')
+    }
+ }
+
+ verificacion()
